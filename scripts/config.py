36e7d373128ddf6446bd385f8a7da86e7fa22d43
Utisse <lorenzopasqui@gmail.com>
2025-02-19 15:19:53 +0100

    changed university dir

--- scripts/config.py
+++ scripts/config.py
@@ -14,5 +14,5 @@ USERCALENDARID = 'primary'
 CURRENT_COURSE_SYMLINK = Path('~/current_course').expanduser()
 CURRENT_COURSE_ROOT = CURRENT_COURSE_SYMLINK.resolve()
 CURRENT_COURSE_WATCH_FILE = Path('/tmp/current_course').resolve()
-ROOT = Path('~/Documenti/polimi/fuori_corso').expanduser()
+ROOT = Path('~/polimi').expanduser()
 DATE_FORMAT = '%a %d %b %Y %H:%M'
