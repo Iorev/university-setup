b6d51fe756d324469f5c08ad69c773d2dd5fe7ed
Utisse <lorenzopasqui@gmail.com>
2025-04-09 11:35:40 +0200

    changed backup from dropbox to owncloud

--- scripts/backup.py
+++ scripts/backup.py
@@ -2,19 +2,21 @@
 import shutil
 from pathlib import Path
 from courses import Courses
-
 # Specifica qui il percorso di destinazione
-DESTINATION_DIR = '/home/lorev/Dropbox/PdfDocs/'
+DESTINATION_DIR = '/home/lorev/ownCloud/appunti pdf/'
 #DESTINATION_DIR.mkdir(parents=True, exist_ok=True)  # Crea la directory se non esiste
 
 for course in Courses():
     lectures = course.lectures
-    #print(f'{course.name}')
-    # Compila il master e controlla il risultato
-    #if lectures.compile_master() == 0:  # Se la compilazione ha successo
-    output_pdf = course.path / 'master.pdf'  # Modifica se il nome del file è diverso
+    r = lectures.parse_range_string('all')
+    lectures.update_lectures_in_master(r)
+    print(f'compilo una volta {course.name}')
+    lectures.compile_master()
+    print(f'compilo una seconda volta {course.name}')
+    lectures.compile_master()
+    output_pdf = course.path / 'master.pdf'  
     if output_pdf.exists():
-        #print(f'{output_pdf}')
+        print(f'Il file {output_pdf} esiste, eseguo il caricamento')
         dst = DESTINATION_DIR + course.name +'.pdf'
         shutil.copyfile(output_pdf, Path(dst).expanduser())
         print(f'File copiato in {dst}')
