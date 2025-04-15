d1f52616241921fbc9ebacd8e7a8edfdd1bd9304
Utisse <lorenzopasqui@gmail.com>
2025-04-15 18:14:56 +0200

    mod for owncloud and compiling twice (and special conf for PF2CFU)

--- scripts/backup.py
+++ scripts/backup.py
@@ -2,26 +2,47 @@
 import shutil
 from pathlib import Path
 from courses import Courses
-# Specifica qui il percorso di destinazione
+import subprocess
 DESTINATION_DIR = '/home/lorev/ownCloud/appunti pdf/'
-#DESTINATION_DIR.mkdir(parents=True, exist_ok=True)  # Crea la directory se non esiste
-
-for course in Courses():
-    lectures = course.lectures
-    r = lectures.parse_range_string('all')
-    lectures.update_lectures_in_master(r)
-    print(f'compilo una volta {course.name}')
-    lectures.compile_master()
-    print(f'compilo una seconda volta {course.name}')
-    lectures.compile_master()
-    output_pdf = course.path / 'master.pdf'  
-    if output_pdf.exists():
-        print(f'Il file {output_pdf} esiste, eseguo il caricamento')
-        dst = DESTINATION_DIR + course.name +'.pdf'
-        shutil.copyfile(output_pdf, Path(dst).expanduser())
-        print(f'File copiato in {dst}')
-    else:
-        print(f'Il file {output_pdf} non esiste.')
-    #else:
-        #print(f'Compilazione fallita per il corso {course.name}.')
-
+DST_FILES_DIR = '/home/lorev/ownCloud/polimi/'
+try:
+    for course in Courses():
+        NTFY_TITLE = f'{course.name}'
+        NTFY_SUBTITLE = 'backup effettuato!'
+        try:
+            if course.name == "PF2CFU":
+                print("Compilo PF2CFU")
+                output_pdf = course.path / 'Presentazione2cfu/main.pdf'
+                result = subprocess.run(
+                    ['pdflatex', '-f', '-interaction=nonstopmode',str(course.path) + '/Presentazione2cfu/main.tex'],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL,
+                    cwd=(str(course.lectures.root))
+                )
+                print(f"RETURN CODE = {result.returncode}")
+                
+            else:
+                lectures = course.lectures
+                r = lectures.parse_range_string('all')
+                lectures.update_lectures_in_master(r)
+                print(f'compilo una volta {course.name}')
+                lectures.compile_master()
+                print(f'compilo una seconda volta {course.name}')
+                lectures.compile_master()
+                output_pdf = course.path / 'master.pdf'
+            if output_pdf.exists():
+                print(f'✅Il file {output_pdf} esiste')
+                dst = DESTINATION_DIR + course.name +'.pdf'
+                shutil.copyfile(output_pdf, Path(dst).expanduser())
+                subprocess.run(['dunstify',"✅" + NTFY_TITLE,NTFY_SUBTITLE])
+            else:
+                subprocess.run(['dunstify',"-b","-u","critical","-t"," 10000","❌" +NTFY_TITLE,"backup failed, file doesn't exists"])
+                print(f'❌Il file {output_pdf} non esiste.')
+            #else:
+                #print(f'Compilazione fallita per il corso {course.name}.')
+    
+        except Exception as e:
+                subprocess.run(['dunstify',"-b","-u","critical","-t","10000","❌"+NTFY_TITLE,f"backup failed! {e}"])
+    
+except Exception as e:
+                subprocess.run(['dunstify',"-b", "-u","critical","-t","10000","❌"+"ERROR!!!",f"backup failed! {e}"])
